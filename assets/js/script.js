17a0e7f377830eefe22a66cac2631f347884fa7b
Michael Muniz <michaelmuniz@michaels-mbp.lan>
2023-03-08 18:03:19 -0600

    added with jQuery the data to the div tags in the html.  add some css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,13 +24,14 @@ function createButtons(){
 //Weather data from local storage based on which city button is clicked
 function getWeather(event){
     event.preventDefault();
-    console.log(event);
+   // console.log(event);
    // console.log(event.target);
-    console.log(event.target.id);
+   // console.log(event.target.id);
     cityID = event.target.id;
     data = localStorage.getItem(cityID);
     data = JSON.parse(data);
      console.log(data);
+     displayWeather(data);
 
 }
 
@@ -58,8 +59,8 @@ function searchCity(event){
            // Put data in string form to local storage
             var holdThis = JSON.stringify(apiResponse);
             localStorage.setItem(city, holdThis)
-
-
+            //pass the data to the display function
+            displayWeather(apiResponse);
             //  make new city buttons on as we go
             createButtons();
         }
@@ -71,25 +72,25 @@ function searchCity(event){
          console.log('This is the data: ');
         var data = JSON.parse(havethis);
         // console.log(data);
+        displayWeather(data);
 
     }
 
 }
-
-
 
 //Get a handle on divs and insert weather data from current data or local stored data
 function displayWeather(data){
 
     //console.log(data);
 
+
     //set data list items that needed (noon for each of the 6 days)
     const dayArray = [2,10,18,26,34,39];
     for(i = 0; i < dayArray.length; i++){
 
          //convert the time and date 
-        const date = new Date(timestamp * 1000); // changed it to milliseconds
         const timestamp = data.list[dayArray[i]].dt; // timestamp in seconds
+        const date = new Date(timestamp * 1000); // changed it to milliseconds
         const month = date.getMonth() + 1; // add 1 to get 1-12 month range
         const day = date.getDate();
         const year = date.getFullYear();
@@ -98,7 +99,7 @@ function displayWeather(data){
 
         // get the weather icon data
         var dataSymbol = data.list[dayArray[i]].weather[0].icon;
-         console.log(dataSymbol);        
+         // console.log(dataSymbol);        
 
        // get the weather icon symbol from the weather site and display it
        var daySymbol =  '<figure><image src="https://openweathermap.org/img/wn/'+dataSymbol+'@2x.png"><figurecaption>'+data.list[dayArray[i]].weather[0].description+'</figurecaption></figure>';
@@ -107,9 +108,15 @@ function displayWeather(data){
        var dayTemp = data.list[dayArray[i]].main.temp;
        var dayWind = data.list[dayArray[i]].wind.speed;
        var dayHumid = data.list[dayArray[i]].main.humidity;
-       console.log(dayTemp);
+      // console.log(dayTemp);
 
+        //get the data into the div tags
+      if(i === 0){
+        $('#day'+[i]).html('<h2>'+data.city.name+' ('+formattedDate+')</h2><p>'+daySymbol+'</p><p>Temp: '+dayTemp+' °F</p><p>Wind: '+dayWind+' MPH</p><p>Humidity: '+dayHumid+' %</p>');
+      }else{
+        $('#day'+[i]).html('<h5>'+formattedDate+'</h5><p>'+daySymbol+'</p><p>Temp: '+dayTemp+' °F</p><p>Wind: '+dayWind+' MPH</p><p>Humidity: '+dayHumid+' %</p>');
 
+        }
     }
 }
 
